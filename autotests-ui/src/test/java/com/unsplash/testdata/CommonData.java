eaa3730967e22ce8cf57f33fcffc0e432af90da1
hoangthach252 <hoangthach2502@gmail.com>
2021-09-01 17:31:44 +0700

    Update CommonData.java

--- autotests-ui/src/test/java/com/unsplash/testdata/CommonData.java
+++ autotests-ui/src/test/java/com/unsplash/testdata/CommonData.java
@@ -18,15 +18,8 @@ public class CommonData {
             .build();
 
     public static Map<String, String[]> paymentCreditCards = ImmutableMap.of(
-            "Visa", new String[]{"11111111111111", "8", "2024", "883", "Zen Auto"},
-            "Master", new String[]{"222222222222222", "6", "2020", "767", "Zen Auto"}
-    );
-
-    public static Map<String, String> sizeOfSFF = ImmutableMap.of(
-            "Self-Fulfill Square Print 10x10", "Medium",
-            "Self-Fulfill Print 5x6", "Small",
-            "self fulfill product USD", "Large",
-            "5x6 self fulfill", "Small"
+            "Visa", new String[]{"11111111111111", "8", "2024", "883", "Auto"},
+            "Master", new String[]{"222222222222222", "6", "2020", "767", "Auto"}
     );
 
 }
